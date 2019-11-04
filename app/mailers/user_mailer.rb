98ce74d2e1cf5a1e4299fbc1127ea2e34275f259
Roman Blondel <rblondel23@gmail.com>
2019-11-04 13:53:25 +0100

    first commit

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,11 @@
+class UserMailer < ApplicationMailer
+  default from: 'no-reply@monsite.fr'
+
+  def welcome_email(user)
+    @user = user
+
+    @url = 'http://monsite.fr/login'
+
+    mail(to: @user.email, subject: 'Bienvenu la famille !')
+  end
+end
