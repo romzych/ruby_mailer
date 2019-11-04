15c53259527aa66a1ca4b2235c4f50c0ee75436a
Roman Blondel <rblondel23@gmail.com>
2019-11-04 14:38:29 +0100

    oui

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,5 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   
   # enable mail sender
-  config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
-  
 end
